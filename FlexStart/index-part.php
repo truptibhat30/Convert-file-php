7dbdd3f3c2e8a79116ae8fe53f87dd4ff4397a8c
truptibhat30 <tkbhat1230@gmail.com>
2023-04-18 01:59:32 -0700

    chnage

--- FlexStart/index-part.php
+++ FlexStart/index-part.php
@@ -7,5 +7,21 @@
 <?php include 'main.php'; ?>
 
 <?php include 'footer.php'; ?> 
+<script>
+    const links= document.querySelectorAll('.nav-link');
+    if(links.length)
+    {
+        links.forEach((link)=>
+        {link.addEventListener('click',(e)=>
+            {links.foeEach((link)=>
+                {link.classList.remove('active');
+        });
+        e.preventDefault();
+        link.classList.add('active');
+    });
+});
+    }
+    </script>
+
 </body>
 </html>
--- FlexStart/main.php
+++ FlexStart/main.php
@@ -12,12 +12,12 @@
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li><a href="blog.php">Blog</a></li>
+          <li><a class="nav-link scrollto" href="index.php#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="index.php#about">About</a></li>
+          <li><a class="nav-link scrollto" href="index.php#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="index.php#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="index.php#team">Team</a></li>
+          <li><a href="blog.php"<?php if(basename($_SERVER['PHP_SELF'])=='blog.php')echo 'class="active"';?>>Blog</a></li>
           <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
@@ -35,8 +35,8 @@
               <li><a href="#">Drop Down 4</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+          <li><a class="nav-link scrollto" href="index.php#contact">Contact</a></li>
+          <li><a class="getstarted scrollto" href="index.php#about">Get Started</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
